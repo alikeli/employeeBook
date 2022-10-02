4f2b689dd5137a348c52480847c9c8f6e92f45b1
alikeli <eli-com2@yandex.ru>
2022-10-02 19:57:28 +0300

    Выполнено 2.5

--- src/main/java/pro/sky/course2/employeebook/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/course2/employeebook/service/EmployeeServiceImpl.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Service;
 import pro.sky.course2.employeebook.Employee.Employee;
 import pro.sky.course2.employeebook.exceptions.EmployeeAlreadyAddedException;
 import pro.sky.course2.employeebook.exceptions.EmployeeNotFoundException;
+import pro.sky.course2.employeebook.exceptions.EmployeeStorageIsFullException;
 
 import java.util.*;
 
 @Service
 
 public class EmployeeServiceImpl implements EmployeeService {
+    int SIZE = 10;
 
     private final Map<String, Employee> employees;
 
@@ -22,6 +24,9 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = new Employee(firstName, lastName);
         if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
+        }
+        if (employees.size() >= SIZE){
+            throw new EmployeeStorageIsFullException();
         }
         employees.put(employee.getFullName(), employee);
         return employee;
