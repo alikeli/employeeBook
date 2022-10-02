a0ad4ab84c2e9ca944469f14882db6516afcf86c
alikeli <eli-com2@yandex.ru>
2022-10-02 19:28:35 +0300

    Выполнено 2.5

--- src/main/java/pro/sky/course2/employeebook/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/course2/employeebook/service/EmployeeServiceImpl.java
@@ -4,35 +4,42 @@ import org.springframework.stereotype.Service;
 import pro.sky.course2.employeebook.Employee.Employee;
 import pro.sky.course2.employeebook.exceptions.EmployeeAlreadyAddedException;
 import pro.sky.course2.employeebook.exceptions.EmployeeNotFoundException;
+import pro.sky.course2.employeebook.exceptions.EmployeeStorageIsFullException;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
 @Service
 
 public class EmployeeServiceImpl implements EmployeeService {
+    private static final int SIZE = 10;
 
     private final List<Employee> employeeList;
 
     public EmployeeServiceImpl() {
-        this.employeeList = new ArrayList<>();
+        this.employeeList = new ArrayList<>(SIZE);
     }
 
     @Override
     public Employee addEmployee(String firstName, String lastName) {
-        Employee employee = new Employee(firstName,lastName);
-        if (employeeList.contains(employee)){
+
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
             throw new EmployeeAlreadyAddedException();
         }
-        employeeList.add(employee);
+        if (employeeList.size() >= SIZE){
+            throw new EmployeeStorageIsFullException();
+        }
+            employeeList.add(employee);
         return employee;
     }
 
     @Override
     public Employee deleteEmployee(String firstName, String lastName) {
-        Employee employee = new Employee(firstName,lastName);
-        if (employeeList.contains(employee)){
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
             employeeList.remove(employee);
             return employee;
         }
@@ -41,8 +48,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee findEmployee(String firstName, String lastName) {
-        Employee employee = new Employee(firstName,lastName);
-        if (employeeList.contains(employee)){
+        Employee employee = new Employee(firstName, lastName);
+        if (employeeList.contains(employee)) {
             return employee;
         }
 
